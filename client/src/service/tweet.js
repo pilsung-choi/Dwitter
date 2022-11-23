469238d0f470e6a3d485c4bc3c6d2686dabce963
pilsung-choi <vlftjd159753@gmail.com>
2022-11-23 21:01:28 +0900

    token from front

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,18 +1,21 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http, tokenStorage) {
+    this.http = http;
+    this.tokenStorage = tokenStorage;
   }
 
   async getTweets(username) {
     const query = username ? `?username=${username}` : '';
     return this.http.fetch(`/tweets${query}`, {
       method: 'GET',
+      headers: this.getHeaders(),
     });
   }
 
   async postTweet(text) {
     return this.http.fetch(`/tweets/`, {
       method: 'POST',
+      header: this.getHeaders(),
       body: JSON.stringify({
         text,
         username: 'ellie',
@@ -24,14 +27,22 @@ export default class TweetService {
   async deleteTweet(tweetId) {
     return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'DELETE',
+      headers: this.getHeaders(),
     });
   }
 
   async updateTweet(tweetId, text) {
     return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'PUT',
-
+      headers: this.getHeaders(),
       body: JSON.stringify({ text }),
     });
+  }
+
+  getHeaders() {
+    const token = this.tokenStorage.getToken();
+    return {
+      Authorization: `Bearer ${token}`,
+    };
   }
 }
