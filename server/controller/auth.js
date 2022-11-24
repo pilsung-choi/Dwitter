42884d41a50c899a20ff5ce033ecae4d8028c15d
pilsung-choi <vlftjd159753@gmail.com>
2022-11-24 14:09:35 +0900

    complete 2 auth

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import {} from 'express-async-errors';
+import 'express-async-errors';
 import * as userRepository from '../data/auth.js';
 
 const jwtSecretKey = '56SQtlwTi$d#4619ca#Q5l*YLbkRjf';
